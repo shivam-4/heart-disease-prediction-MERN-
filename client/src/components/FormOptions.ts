3a52969517fb745fc60ae257bad6459864d57b92
shivam-4 <shivam.72329@gmail.com>
2024-12-29 04:38:12 +0530

    basic frontend added

---
+++ client/src/components/FormOptions.ts
@@ -0,0 +1,41 @@
+export const formOptions = {
+  sex: [
+    { value: 1, label: 'Male' },
+    { value: 0, label: 'Female' }
+  ],
+  cp: [
+    { value: 0, label: 'Typical Angina' },
+    { value: 1, label: 'Atypical Angina' },
+    { value: 2, label: 'Non-anginal Pain' },
+    { value: 3, label: 'Asymptomatic' }
+  ],
+  fbs: [
+    { value: 0, label: '≤ 120 mg/dl' },
+    { value: 1, label: '> 120 mg/dl' }
+  ],
+  restecg: [
+    { value: 0, label: 'Normal' },
+    { value: 1, label: 'ST-T Wave Abnormality' },
+    { value: 2, label: 'Left Ventricular Hypertrophy' }
+  ],
+  exang: [
+    { value: 0, label: 'No' },
+    { value: 1, label: 'Yes' }
+  ],
+  slope: [
+    { value: 0, label: 'Upsloping' },
+    { value: 1, label: 'Flat' },
+    { value: 2, label: 'Downsloping' }
+  ],
+  ca: [
+    { value: 0, label: '0' },
+    { value: 1, label: '1' },
+    { value: 2, label: '2' },
+    { value: 3, label: '3' }
+  ],
+  thal: [
+    { value: 1, label: 'Normal' },
+    { value: 2, label: 'Fixed Defect' },
+    { value: 3, label: 'Reversible Defect' }
+  ]
+};
